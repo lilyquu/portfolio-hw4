0d5f573ac654accbd09757eae928f378415af0cb
y1qu <y1qu@ucsd.edu>
2023-03-01 15:14:02 -0800

    add nativedialogs

---
+++ scripts/native.js
@@ -0,0 +1,6 @@
+const alert = document.getElementById("alert");
+const confirm = document.getElementById("confirm");
+const prompt = document.getElementById("prompt");
+const saferprompt = document.getElementById("saferprompt");
+
+alert.addEventListener("click", alert());
