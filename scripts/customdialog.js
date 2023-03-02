78caa047fe6c04e2508614574c5884e62bb8319a
y1qu <y1qu@ucsd.edu>
2023-03-01 19:13:25 -0800

    update

--- scripts/customdialog.js
+++ scripts/customdialog.js
@@ -6,13 +6,27 @@ const alertDialog = document.getElementById('alertDialog');
 const confirmDialog = document.getElementById('confirmDialog');
 const promptDialog = document.getElementById('promptDialog');
 
+const selectEl = confirmDialog.querySelector('select');
+
 // "Show the dialog" button opens the <dialog> modally
 alertBtn.addEventListener('click', () => {
     alertDialog.showModal();
 });
 
+confirmBtn.addEventListener('click', () => {
+    confirmDialog.showModal();
+});
+
+// "Favorite animal" input sets the value of the submit button
+selectEl.addEventListener('change', (e) => {
+    confirmBtn.value = selectEl.value;
+});
 
 // "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
 alertDialog.addEventListener('close', () => {
-    outputBox.value = `ReturnValue: ${alertDialog.returnValue}.`;
+    outputBox.value = `ReturnValue: ${confirmDialog.returnValue}.`;
+});
+
+confirmDialog.addEventListener('close', () => {
+    outputBox.value = `ReturnValue: ${confirmDialog.returnValue}.`;
 });
