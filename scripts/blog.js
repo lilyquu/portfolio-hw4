f56432b4fa3eaa2dcf1805ee360da1dfb364dd93
y1qu <y1qu@ucsd.edu>
2023-03-01 21:46:12 -0800

    make

--- scripts/blog.js
+++ scripts/blog.js
@@ -8,11 +8,34 @@ const inputBox = document.querySelector('input');
 const btncancel = document.getElementById('btncancel');
 const btncreate = document.getElementById('btncreate');
 
-const input = document.querySelector('input');
-const title = input[0];
-const date = input[1];
-const summary = input[2];
+let input = document.querySelector('input');
+let titlein = input[0];
+let datein = input[1];
+let summaryin = input[2];
+let count = 0;
 
 createBtn.addEventListener('click', () => {
     createDialog.showModal();
 });
+
+btncreate.addEventListener('click', () => {
+    count = count + 1;
+    input = document.querySelector('input');
+    titlein = input[0];
+    datein = input[1];
+    summaryin = input[2];
+    
+    let myPost = {
+        title : titlein,
+        date : datein,
+        summary : summaryin
+    }
+    
+    window.localStorage.setItem(`myPost${count}`, JSON.stringify(myPost));
+    
+    window.localStorage.getItem('myPost1');
+    
+    var temp = document.getElementsById("realpost");
+    var clon = temp.content.cloneNode(true);
+    document.body.appendChild(clon);
+})
