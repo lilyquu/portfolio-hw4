ec17897b0574b30d0ce639289686e91078a9c88e
y1qu <y1qu@ucsd.edu>
2023-03-01 17:25:30 -0800

    update

--- scripts/native.js
+++ scripts/native.js
@@ -4,8 +4,19 @@ const promptbtn = document.getElementById("prompt");
 const saferpromptbtn = document.getElementById("saferprompt");
 
 alertbtn.addEventListener('click', function(){alert("alert")});
+
 confirmbtn.addEventListener('click', function(){
     let text = "Confirm Here.";
     text = "The value returned by the confirm method is : " + confirm(text);
     document.getElementById("output").innerHTML = text;
 });
+
+promptbtn.addEventListener('click', ()=>{
+    let pro = prompt("Please enter your prompt", "Here");
+    if (pro == null){
+        document.getElementById("output").innerHTML = "User didn't enter anything";
+    }
+    else {
+        document.getElementById("output").innerHTML = pro;
+    }
+})
